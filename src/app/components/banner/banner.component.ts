a996602dac7d9e0a3dad6343a32ee0b2f36fa7b9
ernidaniel369 <94999577+ernidaniel369@users.noreply.github.com>
2022-07-30 11:26:23 -0300

    11263007
    banner

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-banner',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
-  constructor() { }
+  per: persona[] = [];
+  
+
+  constructor(private sPersona: PersonaService, private tokenService: TokenService) { }
+  
+
+  isLogged = false;
 
   ngOnInit(): void {
+    this.cargarPersona();
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  cargarPersona(): void {
+    this.sPersona.lista().subscribe(data => { this.per = data; })
+  }
+
+  delete(id?: number){
+    if(id != undefined){
+      this.sPersona.delete(id).subscribe(
+        data => {
+          this.cargarPersona();
+        }, err => {
+          alert("No se pudo borrar");
+        }
+      )
+    }
   }
 
 }
--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -4,11 +4,13 @@ export class persona{
     apellido: String;
     bio: String;
     img: String;
+    banner: String;
 
-    constructor(nombre: String,apellido: String, bio: String, img: String){
+    constructor(nombre: String,apellido: String, bio: String, img: String, banner: String){
         this.nombre = nombre;
         this.apellido = apellido;
         this.bio = bio;
         this.img = img;
+        this.banner = banner;
     }
 }
